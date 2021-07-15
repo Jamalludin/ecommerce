5942f600beedcca2e5577449e2ef099840886d80
jamalludin <jamalludin1209@gmail.com>
2021-07-15 17:37:39 +0700

    add FE

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,22 +1,58 @@
-import { Container } from "react-bootstrap";
-import './App.css';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import {Col, Container, Row} from "react-bootstrap";
+import React from "react";
+import { Header, Footer} from "./components"
+import ListCatagories from "./components/ListCatagories";
+import ListProduct from "./components/ListProduct";
+import {API_URL} from "./utils/constants";
+import axios from "axios";
 
-function App() {
-  return (
-    <>
-        <Header></Header>
+import React, {Component} from 'react';
 
-        <main>
-            <Container>
-                <h1>Welcomer To Warung Shop</h1>
-            </Container>
-        </main>
+class App extends Component {
 
-        <Footer></Footer>
-    </>
-  );
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            menus: [],
+        }
+    }
+
+    componentDidMount() {
+        axios.get(API_URL+"products").then(res => {
+            const menus = res.data;
+            this.setState({ menus })
+        }).catch(error => {
+            console.log("Error Data")
+        })
+    }
+
+    render() {
+        return (
+            <>
+                <div className={App}>
+                    <Header></Header>
+
+                    <div className={'mt-3'}>
+                        <Container fluid>
+                            <Row>
+                                <ListCatagories></ListCatagories>
+                                <Col>
+                                    <h4>
+                                        <strong>Daftar Product</strong>
+                                    </h4>
+                                    <hr />
+                                </Col>
+                                <ListProduct></ListProduct>
+                            </Row>
+                        </Container>
+                    </div>
+
+                    <Footer></Footer>
+                </div>
+            </>
+        );
+    }
 }
 
 export default App;
--- react-frontend/src/components/Header.js
+++ react-frontend/src/components/Header.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import { Navbar, Nav, Container } from "react-bootstrap";
+import {Navbar, Nav, NavDropdown, Form, Button, Container, FormControl} from "react-bootstrap";
 
 const Header = () => {
     return (
         <header>
-            <Navbar bg='primary' variant='drak' expand='lg' collapseOnSelect>
-                <container>
-                    <Navbar.Brand href='#home'>Warung Shop</Navbar.Brand>
-                    <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
-                    <Navbar.Collapse id='basic-navbar-nav'>
-                        <Nav className='ml-auto'>
-                            <Nav.Link>
-                                <i className='fas fa-shopping-cart'></i>
-                                Cart
-                            </Nav.Link>
-                            <Nav.Link>
-                                <i className='fas fa-user'></i>
-                                Link
+            <Navbar expand="lg" variant="dark">
+                <Container>
+                    <Navbar.Brand href={'#home'}>
+                        <strong>Warung</strong> Shop
+                    </Navbar.Brand>
+                </Container>
+                {/*<Container>
+                    <Navbar.Brand href="#home">
+                        <img alt={""} src='../logo.svg'
+                             height={40}
+                             width={40}
+                             className="d-inline-block align-top"/>
+                        Warung Shop
+                    </Navbar.Brand>
+
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider/>
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                            <Form className="d-flex">
+                                <FormControl
+                                    type="search"
+                                    placeholder="Search"
+                                    className="mr-2"
+                                    aria-label="Search"
+                                />
+
+                                <Button variant="outline-success">Search</Button>
+                            </Form>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">More deets</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                Dank memes
                             </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
-                </container>
+                </Container>*/}
             </Navbar>
         </header>
     );
